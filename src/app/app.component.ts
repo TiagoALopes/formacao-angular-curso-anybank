0cfa7ce172f72c264f38271d5567e60909b9b753
Tiago André Lopes <tiago.lopes@serpro.gov.br>
2025-09-19 15:37:50 -0300

    Disponibilização das regras de validação do Tipo Transacao.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
-import { Transacao } from './modelos/transacao';
+import { TipoTransacao, Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,26 @@ import { Transacao } from './modelos/transacao';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  
+export class AppComponent   {
+  transacoes = signal<Transacao[]>([]);
+
+  saldo = computed(() => {
+    return this.transacoes().reduce((accumulador, transacaoAtual) => {
+        switch (transacaoAtual.tipo) {
+          case TipoTransacao.SAQUE:
+            return accumulador - transacaoAtual.valor;
+          case TipoTransacao.DEPOSITO:          
+            return accumulador + transacaoAtual.valor;
+          default:
+            throw new Error("Tipo de transação inválida");
+        }
+    }, 0);
+  });
+
   processarTransacao(transacao: Transacao){
-    console.log(transacao);
-    console.log(`Transação foi criada! com o valor: R$ ${transacao.valor} do Tipo: ${transacao.tipo}`);    
+    if (transacao.tipo === TipoTransacao.SAQUE && transacao.valor > this.saldo()){
+      return alert('Saldo insuficiente!');
+    }
+    this.transacoes.update((listaAtual => [transacao, ...listaAtual]));
   }
 }
